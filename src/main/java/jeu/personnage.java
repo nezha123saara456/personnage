92e04957af47953c12c77ceb673976d51b01c97c
nezha123saara456 <maryatoukart@gmail.com>
2025-12-14 00:49:54 +0100

    implitentoin complet

---
+++ src/main/java/jeu/personnage.java
@@ -0,0 +1,29 @@
+package jeu;
+
+public class Personnage {
+
+    public enum Orientation {
+        NORD, EST, SUD, OUEST
+    }
+
+    private Orientation orientation;
+
+    // Constructeur : le personnage commence toujours NORD
+    public Personnage() {
+        this.orientation = Orientation.NORD;
+    }
+
+    // Méthode tourner
+    public Orientation tourner(int fois) {
+        Orientation[] directions = Orientation.values();
+        int indexActuel = orientation.ordinal();
+        int nouvelIndex = (indexActuel + fois) % directions.length;
+        orientation = directions[nouvelIndex];
+        return orientation;
+    }
+
+    // Getter pour l'orientation (optionnel)
+    public Orientation getOrientation() {
+        return orientation;
+    }
+}
