65f0e8e338b1a5bfb0ce955966e921c2fa865b30
nezha123saara456 <maryatoukart@gmail.com>
2025-12-14 14:36:12 +0100

    Fix tests: match French enum names in PersonnageTest

--- src/test/java/jeu/PersonnageTest.java
+++ src/test/java/jeu/PersonnageTest.java
@@ -5,6 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonnageTest {
 
+  @Test
+    void tournerUneFois() {
+        Personnage p = new Personnage();
+        assertEquals(Personnage.Orientation.EST, p.tourner(1));
+    }
+
+   @Test
+    void tournerDeuxFois() {
+        Personnage p = new Personnage();
+        assertEquals(Personnage.Orientation.SUD, p.tourner(2));
+    }
    @Test
     void tournerQuatreFois() {
         Personnage p = new Personnage();
